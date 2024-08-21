9c87e3003783ce3c5547718f1ca45703ce8a4472
rtj0 <109196548+rtj0@users.noreply.github.com>
2024-08-22 11:56:09 +1200

    Adding in-game Main Menue

---
+++ src/MainMenue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../lib/dependencies.h"
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <iomanip>  
+
+using namespace std;
+
+#ifdef _WIN32
+#include <windows.h>
+
