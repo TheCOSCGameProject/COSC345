fdba01cdbdecb9ab487c067e26a901b053bdd9c2
rtj0 <109196548+rtj0@users.noreply.github.com>
2024-08-22 12:00:47 +1200

    Added printing

--- src/MainMenue.cpp
+++ src/MainMenue.cpp
@@ -11,3 +11,58 @@ using namespace std;
 #ifdef _WIN32
 #include <windows.h>
 
+void SetConsoleSize(int width, int height)
+{
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r);
+
+    MoveWindow(console, r.left, r.top, width, height, TRUE);
+
+    SetWindowLong(console, GWL_STYLE, GetWindowLong(console, GWL_STYLE) & ~WS_SIZEBOX);
+}
+
+#else
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <termios.h>
+
+void TerminalSizej(int height, int width)
+{
+    std::cout << "\e[8;" << height << ";" << width << "t";
+}
+
+#endif
+
+
+void delay(int milliseconds)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    while (std::chrono::high_resolution_clock::now() - start < std::chrono::milliseconds(milliseconds))
+    {
+        
+    }
+}
+
+void typePrint(std::string content, int delayTime = 15, std::string color = "\033[36m")
+{
+    std::string token;
+    std::stringstream ss(content);
+    std::vector<std::string> tokens;
+
+    while (getline(ss, token, '@'))
+    {
+        tokens.push_back(token);
+    }
+
+    for (std::string &text : tokens)
+    {
+        for (char c : text)
+        {
+            std::cout << color << c << std::flush;
+            delay(delayTime);
+        }
+    }
+    std::cout << "\033[0m"; 
+}
+
