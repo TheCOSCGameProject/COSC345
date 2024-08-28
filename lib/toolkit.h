4395c2488369bd0556ba476d554416777695469c
Raaid Taha <tahr475@student.otago.ac.nz>
2024-08-29 10:39:24 +1200

    pushing docs

--- lib/toolkit.h
+++ lib/toolkit.h
@@ -1,4 +1,10 @@
-// toolkit.h
+/*!
+ * @file toolkit.h
+ * @brief Utility functions for the Valeris game.
+ * @details This file contains various utility functions that are used throughout the Valeris game.
+ * These include functions for console manipulation, input handling, string processing, and more.
+ */
+
 #ifndef TOOLKIT_H
 #define TOOLKIT_H
 
@@ -12,7 +18,11 @@
 #ifdef _WIN32
 #include <windows.h>
 
-// Function to set the console size on Windows
+/*!
+ * @brief Set the console size on Windows.
+ * @param width The desired width of the console.
+ * @param height The desired height of the console.
+ */
 void SetConsoleSize(int width, int height);
 
 #else
@@ -20,48 +30,118 @@ void SetConsoleSize(int width, int height);
 #include <unistd.h>
 #include <termios.h>
 
-// Function to set the terminal size on non-Windows systems
+/*!
+ * @brief Set the terminal size on non-Windows systems.
+ * @param height The desired height of the terminal.
+ * @param width The desired width of the terminal.
+ */
 void SetTerminalSize(int height, int width);
 
 #endif
 
-// Function to mimic a delay
+/*!
+ * @brief Mimic a delay in milliseconds.
+ * @param milliseconds The number of milliseconds to delay.
+ */
 void delay(int milliseconds);
 
-// Function to print text with a typing effect
+/*!
+ * @brief Print text with a typing effect.
+ * @param content The text content to print.
+ * @param delayTime The delay time between characters in milliseconds.
+ * @param color The color code for the text.
+ */
 void typePrint(std::string content, int delayTime, std::string color);
 
-// Function to read the content of a file and return it as a string
+/*!
+ * @brief Read the content of a file and return it as a string.
+ * @param fileName The name of the file to read.
+ * @return The content of the file as a string.
+ */
 std::string getFileContent(std::string fileName);
 
-// Function to split a string by a delimiter and return a vector of tokens
+/*!
+ * @brief Split a string by a delimiter.
+ * @param str The string to split.
+ * @param delimiter The character used as the delimiter.
+ * @return A vector of tokens resulting from the split.
+ */
 std::vector<std::string> split(const std::string &str, char delimiter);
 
-// Function to clear the console up to a certain limit
+/*!
+ * @brief Clear the console up to a certain limit.
+ * @param limit The number of lines to clear.
+ */
 void clear(int limit);
 
-// Function to get a full line of user input
+/*!
+ * @brief Get a full line of user input.
+ * @return The user's input as a string.
+ */
 std::string getUserInputLine();
 
-// Function to get a single word or token of user input
+/*!
+ * @brief Get a single word or token of user input.
+ * @return The user's input as a string.
+ */
 std::string getUserInputToken();
 
+/*!
+ * @brief Generate a random number between two values.
+ * @param low The lower bound of the random number.
+ * @param high The upper bound of the random number.
+ * @return A random number between low and high.
+ */
 int generateRandomNumber(int low, int high);
 
+/*!
+ * @brief Convert a string to an integer.
+ * @param str The string to convert.
+ * @return The integer value of the string.
+ */
 int stringToInt(const std::string &str);
 
+/*!
+ * @brief Repeat a string a specified number of times.
+ * @param str The string to repeat.
+ * @param count The number of times to repeat the string.
+ * @return The resulting string after repetition.
+ */
 std::string repeatString(const std::string &str, int count);
 
+/*!
+ * @brief Convert a string to lowercase.
+ * @param str The string to convert.
+ * @return The lowercase version of the string.
+ */
 std::string toLowerCase(const std::string &str);
 
+/*!
+ * @brief Read an integer from user input.
+ * @return The integer value entered by the user.
+ */
 int readInt();
 
+/*!
+ * @brief Disable user input.
+ */
 void disableInput();
 
+/*!
+ * @brief Enable user input.
+ */
 void enableInput();
 
+/*!
+ * @brief Wait for the user to press Enter.
+ */
 void waitForEnter();
 
+/*!
+ * @brief Convert a string to uppercase.
+ * @param input The string to convert.
+ * @return The uppercase version of the string.
+ */
 std::string toUpperCase(const std::string &input);
 
 #endif // TOOLKIT_H
