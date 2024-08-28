8e2a8831131c8a2ed6186f927090e0a07284e6f2
Raaid Taha <tahr475@student.otago.ac.nz>
2024-08-29 10:20:06 +1200

    docs

--- src/valeris.cpp
+++ src/valeris.cpp
@@ -1,3 +1,10 @@
+/*!
+ * @file main.cpp
+ * @brief Main entry point for the game.
+ * @details This file contains the main function, which serves as the entry point for the game. 
+ * It initializes the game, provides menu options, and handles user input to control the game flow.
+ */
+
 #include "../lib/combat.h"
 // #include "../helper/minigames.cpp"
 #include "../lib/player.h"
@@ -6,49 +13,63 @@
 #include "../lib/dungeon.h"
 #include "../lib/menu.h"
 
+/*!
+ * @brief Main function of the game.
+ * @details The main function initializes the game, sets up the console or terminal, 
+ * and displays the main menu. It also handles user input and navigates to different game functionalities.
+ * 
+ * @return Returns 0 upon successful execution.
+ */
 int main()
 {
-  int j;
-  bool running = true;
+  int j; //!< Stores the user's menu selection.
+  bool running = true; //!< Controls the main loop for the game.
 
-  int delayTime = 0;
-  std::string color = "\033[36m";
+  int delayTime = 0; //!< Delay time for displaying text.
+  std::string color = "\033[36m"; //!< Color code for text display.
 
 #ifdef _WIN32
   SetConsoleSize(137, 50);
 #else
   SetTerminalSize(50, 137);
 #endif
+
+  /*!
+   * @brief Main game loop.
+   * @details This loop keeps the game running until the user chooses to exit. 
+   * Depending on the user's input, it can start a new game, load a saved game, 
+   * display instructions, or modify accessibility settings.
+   */
   while (running)
   {
-    Displayj();
-    j = readInt();
+    Displayj(); //!< Display the main menu.
+    j = readInt(); //!< Read user's menu selection.
 
     switch (j)
     {
     case 1:
       clear(10);
-      displayIntro(delayTime, color);
-      StartGame();
+      displayIntro(delayTime, color); //!< Display the game introduction.
+      StartGame(); //!< Start a new game.
       break;
     case 2:
       clear(10);
-      LoadSavedGame();
+      LoadSavedGame(); //!< Load a previously saved game.
       break;
     case 3:
       clear(11);
-      DisplayInstructionsText();
+      DisplayInstructionsText(); //!< Display the instructions for the game.
       break;
     case 4:
       clear(10);
-      Accessiblity(delayTime, color);
+      Accessiblity(delayTime, color); //!< Access and modify accessibility options.
       break;
     case 5:
       std::cout << "Exiting Game. We hope you enjoyed the Game Play." << std::endl;
-      running = false;
+      running = false; //!< Exit the game loop.
       break;
     default:
-      std::cout << "Error: An invalid choice has been entered please try again." << std::endl;
+      std::cout << "Error: An invalid choice has been entered. Please try again." << std::endl;
       delay(2000);
       clear(12);
       break;
